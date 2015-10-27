fb3912ae5fe14a557d000fc4901f83a4507f00f9
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-10-27 20:22:37 +0100

    6

--- client.py
+++ client.py
@@ -13,15 +13,18 @@ import sys
 IP = sys.argv[1]
 PUERTO = int(sys.argv[2])
 LINEA = sys.argv[3:]
-LINEA = " ".join(LINEA)
+if LINEA[0] == "register":
+    mensaje = "REGISTER sip:" + LINEA[1] + " SIP/2.0\r\n\r\n"
+else:
+    mensaje = " ".join(LINEA)
 
 # Creamos el socket, lo configuramos y lo atamos a un servidor/puerto
 my_socket = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 my_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
 my_socket.connect((IP, PUERTO))
 
-print("Enviando: " + LINEA)
-my_socket.send(bytes(LINEA, 'utf-8') + b'\r\n')
+print("Enviando: " + mensaje)
+my_socket.send(bytes(mensaje, 'utf-8') + b'\r\n')
 data = my_socket.recv(1024)
 
 print('Recibido -- ', data.decode('utf-8'))
