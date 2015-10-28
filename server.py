22f267afc693256a108bd19eaac4144beb327625
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-10-28 16:40:39 +0100

    7feo

--- server.py
+++ server.py
@@ -17,7 +17,7 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     def handle(self):
         # Escribe dirección y puerto del cliente (de tupla client_address)
         #self.wfile.write(b"Hemos recibido tu peticion")
-        print(self.client_address)
+        #print(self.client_address)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
@@ -25,12 +25,20 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
             lineutf = lineutf.split(" sip:")
             if lineutf[0] == "REGISTER":
                 address = lineutf[1].split(" ")
+                exp_value = address[-1].split("\r\n")#expire
+                exp_value = exp_value[0]
                 address = address[0]
-                print(address)
-                print(self.client_address[0])
-                self.dicc[address] = [self.client_address[0]]
-                print(self.dicc)
-                self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
+                print(exp_value)
+                if exp_value == "0":
+                    #borrar cliente
+                    del self.dicc[address]
+                    print("borrado")
+                    self.wfile.write(b"borado")
+                else:
+                    #print(self.client_address[0])
+                    self.dicc[address] = [self.client_address[0]]
+                    #print(self.dicc)
+                    self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
 
             # Si no hay más líneas salimos del bucle infinito
