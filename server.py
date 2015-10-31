7ca995d9188e5992602266bc9ff7c2a69357105b
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-10-31 12:00:59 +0100

    8json

--- server.py
+++ server.py
@@ -2,12 +2,20 @@
 # -*- coding: utf-8 -*-
 import socketserver
 import sys
+import time
+import json
 
 
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     dicc = {}
 
+    def register2json(self, nombre):
+        archivo = open(nombre, "w")
+        datjson = json.dump(self.dicc, archivo)
+
     def handle(self):
+        current = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
+        print(current)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
@@ -18,13 +26,16 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 exp_value = address[-1].split("\r\n")
                 exp_value = exp_value[0]
                 address = address[0]
-                #print(exp_value)
                 if exp_value == "0":
-                    del self.dicc[address]#BORRAR
+                    del self.dicc[address]
+                    self.register2json("jsonprueba")
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     print(self.dicc)
                 else:
-                    self.dicc[address] = [self.client_address[0]]#AÑADIR
+                    expires = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + float(exp_value)))
+                    self.dicc[address] = [self.client_address[0], expires]
+                    self.register2json("jsonprueba")
+                    print(self.dicc[address][1])
                     print(self.dicc)
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
