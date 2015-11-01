93d4e81b4e66381dc5bf789a03c4bc89bc5edabb
diegokemp <da.sanchezm@alumnos.urjc.es>
2015-11-01 13:20:19 +0100

    9

--- server.py
+++ server.py
@@ -9,13 +9,29 @@ import json
 class SIPRegisterHandler(socketserver.DatagramRequestHandler):
     dicc = {}
 
+    def json2registered(self):
+        try:
+            archivo2 = open("registered.json", "r")
+            self.dicc = json.load(archivo2)
+            print(self.dicc)
+        except FileNotFoundError:
+            print("No se encontro dicho archivo...")
+
+    def caduca(self, actual):
+        dicc2 = self.dicc.copy()
+        for key in dicc2.keys():
+            if (dicc2[key][1]) < actual:
+                del self.dicc[key]
+                #print(self.dicc)
+
     def register2json(self, nombre):
         archivo = open(nombre, "w")
         datjson = json.dump(self.dicc, archivo)
 
     def handle(self):
+        self.json2registered()
         current = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time()))
-        print(current)
+        self.caduca(current)
         while 1:
             # Leyendo línea a línea lo que nos envía el cliente
             line = self.rfile.read()
@@ -28,14 +44,14 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
                 address = address[0]
                 if exp_value == "0":
                     del self.dicc[address]
-                    self.register2json("jsonprueba")
+                    self.register2json("registered.json")
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
                     print(self.dicc)
                 else:
                     expires = time.strftime('%Y-%m-%d %H:%M:%S', time.localtime(time.time() + float(exp_value)))
                     self.dicc[address] = [self.client_address[0], expires]
-                    self.register2json("jsonprueba")
-                    print(self.dicc[address][1])
+                    self.register2json("registered.json")
+                    #print(self.dicc[address][1])
                     print(self.dicc)
                     self.wfile.write(b"SIP/2.0 200 OK\r\n\r\n")
 
@@ -46,6 +62,8 @@ class SIPRegisterHandler(socketserver.DatagramRequestHandler):
 
 if __name__ == "__main__":
     # Creamos servidor de eco y escuchamos
+    #launch = SIPRegisterHandler()
+    #launch.json2registered()
     serv = socketserver.UDPServer(('', int(sys.argv[1])), SIPRegisterHandler)
     print("Lanzando servidor UDP de eco...")
     serv.serve_forever()
